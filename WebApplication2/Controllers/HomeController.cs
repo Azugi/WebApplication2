3eeabc3b9ec823605aaa36415c707c4d7efebdb0
Azugi <62372681+Azugi@users.noreply.github.com>
2020-12-19 00:16:10 +0300

    Initial commit

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NewsSite.Models;
+
+namespace NewsSite.Controllers
+{
+    public class HomeController : Controller
+    {
+        NewsContext db = new NewsContext();
+        public ActionResult Index()
+        {
+            
+            return View(db.News);
+        }
+        
+        public ActionResult Browse(int Id)
+        {
+            News BrowseNews = db.News
+                .Where(i => i.Id == Id)
+                .FirstOrDefault();
+            ViewBag.Browsing = BrowseNews;
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public PartialViewResult Add(News newsAdd)
+        {
+            ViewBag.Message = "news add";
+            db.News.Add(newsAdd);
+            db.SaveChanges();
+            return PartialView("_PartialAlertAdd");
+
+        }
+        [HttpGet]
+        public ActionResult Delete(int Id)
+        {
+            News DelNews = db.News
+                .Where(i => i.Id == Id)
+                .FirstOrDefault();
+            ViewBag.Browsing = DelNews;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Delete(int Id,string str)
+        {
+            News DelNews = db.News
+                .Where(i => i.Id == Id)
+                .FirstOrDefault();
+            db.News.Remove(DelNews);
+            db.SaveChanges();
+            return View("Del");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/Models/NewsContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace NewsSite.Models
+{
+    public class NewsContext: DbContext
+    {
+        public DbSet<News> News { get; set; }
+    }
+}
